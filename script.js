4e808906b3e21eb499689435dca782e26174ea52
Vaibhav21421 <112789816+Vaibhav21421@users.noreply.github.com>
2024-04-09 21:34:56 +0530

    gendu

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-gsap.from("#pg2 img",{
-    width:"20%",
+gsap.to("#pg1 img",{
+    width:"100%",
+    top:0,
     delay:2,
     duration:2,
     scrollTrigger:{
         scroller:"body",
-        trigger:"#pg2",
+        trigger:"#pg1",
         markers:true,
         scrub:2,
         // top:0, 
-        start:"top 10%",
-        end:"top 0",
+        start:"top 0%",
+        end:"top -100%",
         pin:true
 
 
