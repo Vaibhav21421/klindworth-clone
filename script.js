6196ca4f558403a197b5680f769672306a609bf2
Vaibhav21421 <112789816+Vaibhav21421@users.noreply.github.com>
2024-04-10 19:40:36 +0530

    gendu

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const nav_switch = document.querySelector("#nav-switch");
+
+const line1 = document.querySelector("#nav-switch #line1");
+const line2 = document.querySelector("#nav-switch #line2");
+
+
+
+nav_switch.addEventListener("mouseenter",()=>{
+    console.log("hi");
+    line2.style.transform = "rotate(-90deg)";
+    
+    nav_switch.style.width = "4vw";
+    nav_switch.style.borderRadius = "50%"; 
+
+
+    
+    // line1.style.position="absolute";
+    // line1.style.marginTop="20%";
+})
+
+nav_switch.addEventListener("mouseleave",()=>{
+    console.log("bi")
+    line2.style.transform = "rotate(0deg)";
+    nav_switch.style.borderRadius = "25px"; 
+
+    nav_switch.style.width = "9.7vw";
+
+   
+
+
+    // line1.style.position="inherit";
+    // line1.style.marginTop="initial";
+})
+
+
+
+
+
 gsap.to("#pg1 img",{
     width:"100%",
     top:0,
@@ -6,8 +44,8 @@ gsap.to("#pg1 img",{
     scrollTrigger:{
         scroller:"body",
         trigger:"#pg1",
-        markers:true,
-        scrub:2,
+        // markers:true,
+        scrub:1,
         // top:0, 
         start:"top 0%",
         end:"top -100%",
